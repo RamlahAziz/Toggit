39efe1c5e6eef9da1a5b0509491906c10fdf669d
miaaaahmed97 <51957914+miaaaahmed97@users.noreply.github.com>
2019-10-12 02:59:54 +0500

    user registration

---
+++ app/src/main/java/com/strigiformes/teletalk/CustomObjects/User.java
@@ -0,0 +1,11 @@
+package com.strigiformes.teletalk.CustomObjects;
+
+public class User {
+
+    private String name;
+    private String profilePicture;
+    private String uid;
+    private String deviceToken;
+
+
+}
---
+++ app/src/main/java/com/strigiformes/teletalk/Service/MyFirebaseMessagingService.java
@@ -0,0 +1,105 @@
+package com.strigiformes.teletalk.Service;
+
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Context;
+import android.content.Intent;
+import android.graphics.Color;
+import android.media.RingtoneManager;
+import android.net.Uri;
+import android.os.Build;
+import android.util.Log;
+
+import com.google.firebase.messaging.FirebaseMessagingService;
+import com.google.firebase.messaging.RemoteMessage;
+import com.strigiformes.teletalk.Home;
+import com.strigiformes.teletalk.R;
+import com.strigiformes.teletalk.Startup.WelcomeActivity;
+
+import androidx.core.app.NotificationCompat;
+
+public class MyFirebaseMessagingService extends FirebaseMessagingService {
+
+    private static final String TAG = "MyFirebaseMessaging";
+    private static int NOTIFICATION_ID = 1;
+
+    @Override
+    public void onMessageReceived(RemoteMessage remoteMessage) {
+        super.onMessageReceived(remoteMessage);
+
+        generateNotification(remoteMessage.getData().get("title"),
+                remoteMessage.getData().get("body"), remoteMessage.getData().get("click_action"));
+
+
+    }
+
+    private void generateNotification(String title, String body, String click_action) {
+
+        //Intent intent = new Intent(this, MainActivity.class);
+        Intent intent;
+        if(click_action.equals("WELCOME")){
+            intent = new Intent(this, WelcomeActivity.class);
+            intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+        }
+        else{
+            intent = new Intent(this, Home.class);
+            intent.putExtra("OPEN_TAB", "posted");
+            intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+        }
+
+
+        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+
+        PendingIntent pendingIntent = PendingIntent.getActivity(this, 0 , intent, PendingIntent.FLAG_ONE_SHOT);
+
+        Uri soundUri = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION);
+
+        NotificationCompat.Builder notificationBuilder = new NotificationCompat.Builder(this, "com.rasai.customerBooking.service.test")
+                .setSmallIcon(R.mipmap.ic_launcher)
+                .setContentTitle(title)
+                .setContentText(body)
+                .setPriority(NotificationCompat.PRIORITY_HIGH)
+                .setSound(soundUri)
+                .setVibrate(new long[]{100, 200, 300, 400, 500, 400, 300, 200, 400})
+                .setContentIntent(pendingIntent)
+                .setAutoCancel(true);
+
+        NotificationManager notificationManager = (NotificationManager)getSystemService(Context.NOTIFICATION_SERVICE);
+        String NOTIFICATION_CHANNEL_ID = "com.rasai.customerBooking.service.test";
+
+        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.O){
+
+            NotificationChannel notificationChannel = new NotificationChannel(NOTIFICATION_CHANNEL_ID, "Notification", NotificationManager.IMPORTANCE_DEFAULT);
+            notificationChannel.setDescription("EDMT Channel");
+            notificationChannel.enableLights(true);
+            notificationChannel.setLightColor(Color.BLUE);
+            notificationChannel.setVibrationPattern(new long[]{0, 1000, 500, 1000});
+            notificationChannel.enableLights(true);
+            notificationManager.createNotificationChannel(notificationChannel);
+        }
+
+        if(NOTIFICATION_ID>1073741824){
+            NOTIFICATION_ID = 0;
+        }
+
+        notificationManager.notify(NOTIFICATION_ID++, notificationBuilder.build());
+
+    }
+
+    @Override
+    public void onNewToken(String s) {
+        super.onNewToken(s);
+
+        Log.d("FirebaseMessagingTag", s);
+        String deviceToken = null;
+
+
+    }
+
+    @Override
+    public void onDeletedMessages() {
+        super.onDeletedMessages();
+    }
+
+}
